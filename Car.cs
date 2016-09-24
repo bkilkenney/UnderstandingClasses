bc2220f48faa70264c0b180f10b52cc63a3dddcb
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-24 19:47:22 -0400

    Create class containing methods, properties and an instance

---
+++ Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingClasses
+{
+    class Car
+    {
+        //Properties - All cars have these properties
+
+        public string Make { get; set; }   
+        public string Model { get; set; }
+        public int Year { get; set; }       //You can type "Prop-tab-tab" and it sets up the property for you
+        public string Color { get; set; } 
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;  //This method must return a decimal above, so we declare: decimal carValue here, that's what it will return
+
+            if (Year > 1990)
+                carValue = 10000;
+            else
+                carValue = 2000;
+
+            return carValue;          //Returning carValue, as stated above
+        }
+        
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,33 @@ namespace UnderstandingClasses
     {
         static void Main(string[] args)
         {
+            //Creating an instance of an object(creating a car, in this example)
+
+            Car instanceOfCar = new Car();     //Need to set the properties of this particular car, and you might want to get that info out
+
+            instanceOfCar.Make = "Toyota";
+            instanceOfCar.Model = "Camry";
+            instanceOfCar.Year = 2000;          ////SETTING this car's properties
+            instanceOfCar.Color = "Beige";      //You SET a properties value with the assignment operator =
+
+            Console.WriteLine("{0}, {1}, {2}, {3}",         //Using placeholders, below determines what each placeholder is holding
+                instanceOfCar.Make, 
+                instanceOfCar.Model, 
+                instanceOfCar.Year, 
+                instanceOfCar.Color);           //GETTING this car's properties
+
+            //decimal value = DetermineMarketValue(instanceOfCar); //Calling the method from below with the car parameters above
+            // Console.WriteLine("{0:C}", value);
+
+            Console.WriteLine("{0:C}", instanceOfCar.DetermineMarketValue());
+            
+            Console.ReadLine();           
+        }
+
+        private static decimal DetermineMarketValue(Car car)  //Creating a method with parameters
+        {
+            decimal carValue = 100.00M;
+            return carValue;
         }
     }
 }
